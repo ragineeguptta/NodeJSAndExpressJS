eeb154b8fbd23bd766d65c8307c3dec3adfbfbd2
Raginee <raginee.gupta@hirdhav.com>
2025-01-01 11:13:46 +0530

    done

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,1 +1,20 @@
-console.log(" Hello From Node !");
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+//CommonJS
+
+import generateName from "sillyname";
+import superHeroName from "superhero";
+import superheros from "superheroes";
+
+var sillyName = generateName();
+console.log(`My name is ${sillyName}.`);
+
+// Use 'new' to instantiate the superhero name
+var superHero = new superHeroName();
+console.log(`I am ${superHero}!`);
+
+
+const name = superheros.random;
+console.log(`I am ${name}!`);
